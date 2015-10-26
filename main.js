c1b53c73c374accdeb1afd28a7b711bc5fbac469
Matias Klemola <matias.klemola@gmail.com>
2015-10-26 18:14:54 +0200

    added server

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 'use strict';
 
-let yaml = require('js-yaml');
-let fs   = require('fs');
+const yaml = require('js-yaml');
+const fs   = require('fs');
+const http = require('http');
 
 //npm link to data module and constant filenames are assumed
-const dataPath = './node_modules/cvdata/';
-const cvFilePath = dataPath + 'cv_english.yml';
-const skillsFilePath = dataPath + 'skills.yml';
+const DATA_PATH = './node_modules/cvdata/';
+const CV_FILE_PATH = DATA_PATH + 'cv_english.yml';
+const SKILLS_FILE_PATH = DATA_PATH + 'skills.yml';
+const PORT = 3000;
 
 let cv;
 let skills;
 
-try {
-  cv = yaml.safeLoad(fs.readFileSync(cvFilePath, 'utf8'))
-  skills = yaml.safeLoad(fs.readFileSync(skillsFilePath, 'utf8'))
-} catch (e) {
-  console.log("error", e);
-}
+const server = http.createServer(function (request, response) {
+  response.end(JSON.stringify({
+    cv: cv,
+    skills: skills
+  }))
+});
 
-console.log(Object.keys(cv));
-console.log(Object.keys(skills));
+server.listen(PORT, () => {
+  try {
+    cv = yaml.safeLoad(fs.readFileSync(CV_FILE_PATH, 'utf8'));
+    skills = yaml.safeLoad(fs.readFileSync(SKILLS_FILE_PATH, 'utf8'));
+    console.log('Server started on port', PORT)
+  } catch (e) {
+    server.close(() => console.log(e))
+  }
+});
